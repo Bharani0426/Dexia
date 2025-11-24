719e36a6a6f0dc48878b0b7b6cde9c0617a10535
Bharani <136856115+Bharani0426@users.noreply.github.com>
2025-11-24 15:00:36 +0100

    test

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,6 @@ import { Stack } from 'expo-router';
 import './global.css';
 
 export default function RootLayout() {
-<<<<<<< HEAD
   return <Stack />;
 }gggg
-=======
-  return <Stack />;  
-}
->>>>>>> 70a0dad85fc78d4e8a5b53922056f78140c40737
+
